be3e99f0a8269a18a0c745e619d935689a184daf
eliotorga <eliotorga.iut@gmail.com>
2026-01-14 15:00:47 +0100

    config

--- includes/config.php
+++ includes/config.php
@@ -3,11 +3,6 @@
 // contient la connexion a la base de donnees et les identifiants de connexion
 // ce fichier est inclus partout dans le projet
 
-
-/* ---------------------------
-   CONFIGURATION DE SÉCURITÉ
-----------------------------*/
-
 // Configuration du hachage des mots de passe
 define('PASSWORD_ALGO', PASSWORD_BCRYPT);
 define('PASSWORD_OPTIONS', [
@@ -15,30 +10,17 @@ define('PASSWORD_OPTIONS', [
 ]);
 
 
-/* ---------------------------
-   IDENTIFIANTS DE CONNEXION
-   (exigence du professeur : PAS DE TABLE utilisateur)
-----------------------------*/
-
 $AUTH_LOGIN = "admin";  // Nom d'utilisateur
 // Mot de passe haché (bcrypt). Générer avec: htpasswd -nbBC 12 admin 'motdepasse'
 $AUTH_PASSWORD_HASH = '$2y$12$rPJltJboyCt8h9q33Y0Olee7NheJZkO3Cw7y/T7w3ii7uL8FkTFwm';
 
 
-/* ---------------------------
-   PARAMÈTRES BASE DE DONNÉES
-----------------------------*/
-
 $DB_HOST = "localhost";
 $DB_NAME = "gestion_equipe";
 $DB_USER = "root";
-$DB_PASS = "";  // sous XAMPP / WAMP, souvent vide
+$DB_PASS = "";
 
-
-/* ---------------------------
-   CONNEXION PDO SÉCURISÉE
-----------------------------*/
-
+I
 try {
     $gestion_sportive = new PDO(
         "mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8",
@@ -54,10 +36,6 @@ try {
     die("❌ Erreur de connexion à la base de données : " . $e->getMessage());
 }
 
-
-/* ---------------------------
-   SÉCURITÉ SESSION
-----------------------------*/
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
